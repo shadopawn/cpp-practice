09aa773a261a279a453b3b62c04416b65c0207b7
Daniel Litt <daniel_litt@comcast.net>
2021-05-15 22:47:38 -0400

    Add the word value to print statements

--- pointer-practice/PointerPractice.cpp
+++ pointer-practice/PointerPractice.cpp
@@ -5,14 +5,14 @@ int main() {
     int &reference = original;
     int* pointer = &original;
 
-    std::cout << "original: " << original << std::endl;
+    std::cout << "original value: " << original << std::endl;
     std::cout << "original address: " << &original << std::endl;
     std::cout << std::endl;
 
-    std::cout << "reference: " << reference << std::endl;
+    std::cout << "reference value: " << reference << std::endl;
     std::cout << "reference address: " << &reference << std::endl;
     std::cout << std::endl;
 
-    std::cout << "pointer: " << *pointer << std::endl;
+    std::cout << "pointer value: " << *pointer << std::endl;
     std::cout << "pointer address: " << pointer << std::endl;
 }
